a6cae024514537c21866c7ae76eece0eca01929a
Jason Lu <jasonlu2907@gmail.com>
2020-11-08 11:38:25 -0600

    Adding navigation bar, all layout using bootstrap 3, creating Recipe model, displaying data

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from 'src/app/share/recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Yellow birthday cupcakes', 'Sour cream, cake flour', 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/05/yellow-birthday-cupcakes-chocolate-frosting-600x900.jpg')
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
