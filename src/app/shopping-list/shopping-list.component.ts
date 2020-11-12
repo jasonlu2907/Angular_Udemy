a73de0881447ff1c21d46f96c14976bb9fbf1feb
Jason Lu <jasonlu2907@gmail.com>
2020-11-11 22:27:55 -0600

    Recipes detail, shopping list, shopping-list edit, Ingredients model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../share/ingredients.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('trung', 5),
+    new Ingredient('ca chua', 12)
+  ];
 
   constructor() { }
 
